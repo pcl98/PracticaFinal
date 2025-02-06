0825e106222df539020a9cf4f1875ba10374e9b0
jruedawp <jesuscheca2@gmail.com>
2025-02-06 20:15:49 +0100

    prrimer commit navegacion

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavegacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
