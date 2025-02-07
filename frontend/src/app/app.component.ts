62061ad23a336ec0b090d93cefc6ff0d09abadc0
jruedawp <jesuscheca2@gmail.com>
2025-02-07 20:09:36 +0100

    segundo commit navegacion

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavegacionComponent],
+  imports: [RouterOutlet, NavegacionComponent,InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
