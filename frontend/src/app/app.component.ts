eff99af0a160edee324930bd030c4c3a14417382
pcl98 <pablo.cerdanlopez@gmail.com>
2025-02-08 21:44:15 +0100

    Actualizada la cabecera y primer modelo y controlador de la API

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { CabeceraComponent } from "./components/cabecera/cabecera.component";
+import { NotificacionesComponent } from "./components/notificaciones/notificaciones.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavegacionComponent],
+  imports: [RouterOutlet, NavegacionComponent, CabeceraComponent, NotificacionesComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  mostrarVentanaNotificaciones = false;
+
+  mostrarNotificaciones() {
+    this.mostrarVentanaNotificaciones = true;
+  }
 }
