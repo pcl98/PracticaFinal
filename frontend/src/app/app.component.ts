535483187a608417b6591c0d3610cf8a8e773b31
jruedawp <jesuscheca2@gmail.com>
2025-02-09 14:13:26 +0100

    cambiando el fondo y creando titulo (esta comentado)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,10 +9,11 @@ import { ProfesoresComponent } from './components/profesores/profesores.componen
 import { NuestraHistoriaComponent } from './components/nuestra-historia/nuestra-historia.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { PreguntasFrecuentesComponent } from './components/preguntas-frecuentes/preguntas-frecuentes.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavegacionComponent,InicioComponent,PresencialComponent, OnlineComponent,CalendarioComponent,ProfesoresComponent,NuestraHistoriaComponent,ContactoComponent,PreguntasFrecuentesComponent ],
+  imports: [RouterOutlet, NavegacionComponent,InicioComponent,PresencialComponent, OnlineComponent,CalendarioComponent,ProfesoresComponent,NuestraHistoriaComponent,ContactoComponent,PreguntasFrecuentesComponent, CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/cabecera/cabecera.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  imports: [],
+  templateUrl: './cabecera.component.html',
+  styleUrl: './cabecera.component.css'
+})
+export class CabeceraComponent {
+
+}
