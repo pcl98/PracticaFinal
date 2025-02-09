f14619e3fbbf6ff795d23f92b26e3d57f933c32c
pcl98 <pablo.cerdanlopez@gmail.com>
2025-02-09 21:28:08 +0100

    Correcciones en el enrutamiento de la api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/api/usuarios', UsuarioController::class);
+Route::resource('/api/usuarios', UsuarioController::class);*/
